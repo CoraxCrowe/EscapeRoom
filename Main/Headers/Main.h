9225700053ce97c226598a7222a2c5f46e52fb57
CoraxCrowe <coraxcrowe@gmail.com>
2023-09-14 00:37:46 +0200

    Add rooms and doors arrays, and printAdjacentRooms utility function

--- Main/Headers/Main.h
+++ Main/Headers/Main.h
@@ -5,4 +5,9 @@
 
 #include "Door.h"
 
+extern Room* rooms[3];
+extern Door* doors[][3];
+
+void printAdjacentRooms(int roomIndex);
+
 #endif
--- Main/Sources/Main.cpp
+++ Main/Sources/Main.cpp
@@ -2,15 +2,25 @@
 
 #include "Main.h"
 
+Room *rooms[3]{new Room(0), new Room(1), new Room(2)};
+Door *doors[][3]{
+    {new Door(rooms[0], rooms[1]), nullptr, nullptr},
+    {new Door(rooms[1], rooms[0]), new Door(rooms[1], rooms[2]), nullptr},
+    {new Door(rooms[2], rooms[1]), nullptr, nullptr}};
+
+int currentRoomIndex = 0;
+
+void printAdjacentRooms(int roomIndex) {
+  for (int i = 0; i < 3; i++){
+    if (doors[roomIndex][i] == nullptr) {
+      continue;
+    }
+    std::cout << "There is a door towards room " << doors[roomIndex][i]->targetRoom->roomNumber << "\n";
+  }
+}
+
 int main()
 {
-
-  Room* room1 = new Room(1);
-  Room* room2 = new Room(2);
-
-  Door* door = new Door(room1, room2);
-
-  std::cout << door->sourceRoom->roomNumber;
-
+  printAdjacentRooms(1);
   return 0;
 }
